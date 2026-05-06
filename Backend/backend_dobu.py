96419ce34cccc43acbcea59eb1437cc0028d73c9
Godwin Alamu <alamugodwin@gmail.com>
2026-05-06 20:20:43 +0100

    fix: comment out unused imports in backend_dobu.py

--- Backend/backend_dobu.py
+++ Backend/backend_dobu.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 import os
 import os.path
-from dotenv import load_dotenv
-from firebase_admin import credentials, firestore, initialize_app   
-import requests
+#from dotenv import load_dotenv
+#from firebase_admin import credentials, firestore, initialize_app   
+#import requests
 app = Flask(__name__)
 CORS(app, origins=["https://dobu-martial-class-ln7q.vercel.app"])
 @app.route('/')
